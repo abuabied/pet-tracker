9ba4680db36563cc71ef762cb31382312b999738
shhabmlkawy <102469926+shhabmlkawy@users.noreply.github.com>
2024-07-20 14:01:04 +0300

    pages initial state

--- pettracker_frontend/src/components/header/desktop/DesktopMenuBar.jsx
+++ pettracker_frontend/src/components/header/desktop/DesktopMenuBar.jsx
@@ -9,6 +9,12 @@ import { useNavigate } from "react-router-dom";
 import { scrollToTop } from "../../../helpers/setWindowSize";
 import InfoIcon from "@mui/icons-material/Info";
 import HomeIcon from "@mui/icons-material/Home";
+import PetsIcon from '@mui/icons-material/Pets';
+import DoorSlidingIcon from '@mui/icons-material/DoorSliding';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import { isLogged } from "../../../helpers/helperFunctions";
+import { REGISTRATION_MESSAGES } from "../../../consts/StringConsts";
+import { toast } from "react-toastify";
 
 export const DesktopMenuBar = () => {
     const navigate = useNavigate();
@@ -16,6 +22,16 @@ export const DesktopMenuBar = () => {
         scrollToTop();
         navigate("/");
     };
+    const goToPage = (pageName) => {
+        if (isLogged()) {
+          scrollToTop();
+          navigate(`${"/" + pageName}`);
+        } else {
+          toast.warning(
+            REGISTRATION_MESSAGES.GENERAL_MESSAGES.LOGIN_BEFORE_ACTION
+          );
+        }
+      };
     const goToHelp = () => {
         scrollToTop();
         navigate("/help");
@@ -23,18 +39,45 @@ export const DesktopMenuBar = () => {
 
     return (
         <List sx={{ display: 'flex', flexDirection: 'row', gap: '1rem' }}>
-            <ListItem sx={{margin: "0", padding: "0"}}> 
-                <ListItemButton onClick={goToHome}  sx={{margin: "0"}}>
-                    <ListItemIcon sx={{margin: "0 -1.5rem 0 0"}}>
+            <ListItem sx={{ margin: "0", padding: "0" }}>
+                <ListItemButton onClick={goToHome} sx={{ margin: "0" }}>
+                    <ListItemIcon sx={{ margin: "0 -1.5rem 0 0" }}>
                         <HomeIcon sx={{ color: "black" }} />
                     </ListItemIcon>
                     <ListItemText primary="Home" />
                 </ListItemButton>
             </ListItem>
 
-            <ListItem sx={{margin: "0", padding: "0"}}>
-                <ListItemButton onClick={goToHelp}  sx={{margin: "0"}}>
-                    <ListItemIcon sx={{margin: "0 -1.5rem 0 0"}}>
+            <ListItem sx={{ margin: "0", padding: "0" }}>
+                <ListItemButton onClick={() => goToPage("pets")} sx={{ margin: "0" }}>
+                    <ListItemIcon sx={{ margin: "0 -1.5rem 0 0" }}>
+                        <PetsIcon sx={{ color: "black" }} />
+                    </ListItemIcon >
+                    <ListItemText primary="Pets" />
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem sx={{ margin: "0", padding: "0" }}>
+                <ListItemButton onClick={() => goToPage("visits")} sx={{ margin: "0" }}>
+                    <ListItemIcon sx={{ margin: "0 -1.5rem 0 0" }}>
+                        <DoorSlidingIcon sx={{ color: "black" }} />
+                    </ListItemIcon >
+                    <ListItemText primary="Visits" />
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem sx={{ margin: "0", padding: "0" }}>
+                <ListItemButton onClick={() => goToPage("clinics")} sx={{ margin: "0" }}>
+                    <ListItemIcon sx={{ margin: "0 -1.5rem 0 0" }}>
+                        <LocalHospitalIcon sx={{ color: "black" }} />
+                    </ListItemIcon >
+                    <ListItemText primary="Clinics" />
+                </ListItemButton>
+            </ListItem>
+
+            <ListItem sx={{ margin: "0", padding: "0" }}>
+                <ListItemButton onClick={goToHelp} sx={{ margin: "0" }}>
+                    <ListItemIcon sx={{ margin: "0 -1.5rem 0 0" }}>
                         <InfoIcon sx={{ color: "black" }} />
                     </ListItemIcon >
                     <ListItemText primary="Help" />
